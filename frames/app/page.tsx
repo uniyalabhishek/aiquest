53d9946f9dcc96112fdaaed8f26cfca448b598ce
taijusanagi <taijusanagi@gmail.com>
2024-03-23 12:32:02 +0900

    use old version of frame.js to use state

--- frames/app/page.tsx
+++ frames/app/page.tsx
@@ -1,28 +1,72 @@
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameInput,
+  FrameReducer,
+  NextServerPageProps,
+  getFrameMessage,
+  getPreviousFrame,
+  useFramesReducer,
+} from "frames.js/next/server";
 import Link from "next/link";
-import { currentURL, vercelURL } from "./utils";
-import { createDebugUrl } from "./debug";
-import type { Metadata } from "next";
-import { fetchMetadata } from "frames.js/next";
+import { currentURL } from "./utils";
+import { DEFAULT_DEBUGGER_HUB_URL, createDebugUrl } from "./debug";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "New api example",
-    description: "This is a new api example",
-    other: {
-      ...(await fetchMetadata(new URL("/frames", vercelURL() || "http://localhost:3000"))),
-    },
-  };
-}
+type State = {
+  messages: string[];
+};
 
-export default async function Home() {
-  const url = currentURL("/");
+const initialState: State = { messages: [] };
+
+// This is a react server component only
+export default async function Home({ searchParams }: NextServerPageProps) {
+  const url = currentURL("/examples/transaction");
+  const previousFrame = getPreviousFrame<State>(searchParams);
+
+  // const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
+
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
+  });
+  console.log(frameMessage?.inputText);
+
+  // if (frameMessage?.transactionId) {
+  //   return (
+  //     <FrameContainer
+  //       pathname="/examples/transaction"
+  //       postUrl="/examples/transaction/frames"
+  //       state={state}
+  //       previousFrame={previousFrame}
+  //     >
+  //       <FrameImage aspectRatio="1:1">
+  //         <div tw="bg-purple-800 text-white w-full h-full justify-center items-center flex">
+  //           Transaction submitted! {frameMessage.transactionId}
+  //         </div>
+  //       </FrameImage>
+  //       <FrameInput text="put some text here" />
+  //       <FrameButton action="link" target={`https://optimistic.etherscan.io/tx/${frameMessage.transactionId}`}>
+  //         View on block explorer
+  //       </FrameButton>
+  //     </FrameContainer>
+  //   );
+  // }
+
+  // then, when done, return next frame
+  const reducer = (state: any, action: any) => ({ messages: [...state.messages, frameMessage?.inputText] });
+  const [state, dispatch] = useFramesReducer(reducer, { messages: [] }, previousFrame);
+  console.log(state);
 
   return (
     <div>
-      Rent farcaster storage example{" "}
-      <Link href={createDebugUrl(url)} className="underline">
-        Debug
-      </Link>
+      Rent farcaster storage example <Link href={createDebugUrl(url)}>Debug</Link>
+      <FrameContainer pathname="/" postUrl="/frames" state={state} previousFrame={previousFrame}>
+        <FrameImage aspectRatio="1:1">
+          <div tw="bg-purple-800 text-white w-full h-full justify-center items-center">Rent farcaster storage</div>
+        </FrameImage>
+        <FrameInput text="put some text here" />
+        <FrameButton onClick={dispatch}>Test</FrameButton>
+      </FrameContainer>
     </div>
   );
 }
