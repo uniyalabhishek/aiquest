a7ce138e53ca6d521918d0fe38d208fee3ea440f
taijusanagi <taijusanagi@gmail.com>
2024-03-23 16:33:34 +0900

    conversation is ok

---
+++ frames/app/data.ts
@@ -0,0 +1,24 @@
+export const originalPrompt = `
+## Your role
+
+You are a dark fantasy TRPG game master, and please guide the user to reach the boss area.
+
+## Areas
+
+This TRPG contains 3 areas, a bar, a church, and a castle.
+
+The bar is where users can gather information by talking with others. They let users know the church is the place to gather the new power to defeat the boss, and the castle is the place to beat the boss. It consists of an entrance and inside.
+
+The church is a place to gather power to beat the boss. It consists of an entrance and inside.
+
+The castle is the place to beat the boss; it consists of an entrance, and inside.
+
+## Output format
+
+Please always respond using the following format to replay; your response is always in markdown format.
+
+- Please add a description of where the user is, and try to describe the detail of the image as user can visualize it with around 160 character
+
+---
+<description here around 160 characters>
+`;
--- frames/app/frames/route.tsx
+++ frames/app/frames/route.tsx
@@ -5,6 +5,7 @@ import { Button } from "frames.js/next";
 import { createFrames } from "frames.js/next";
 import { kv } from "@vercel/kv";
 import OpenAI from "openai";
+import { originalPrompt } from "../data";
 
 const frames = createFrames({
   basePath: "/frames",
@@ -36,16 +37,24 @@ const handleRequest = frames(async (ctx) => {
   console.log("sessionKey", sessionKey);
   console.log("process.env.OPENAI_API_KEY", process.env.OPENAI_API_KEY);
   console.log("ctx?.message?.inputText", ctx?.message?.inputText);
-  let text = "";
+  const inputText = ctx?.message?.inputText;
+  let responseText = "";
   if (sessionKey) {
-    await kv.set(sessionKey, "test");
-    const sessionData = await kv.get("test");
-    console.log("sessionData", sessionData);
+    const messages: any = [];
+    const sessionData = await kv.get(sessionKey);
+    if (!sessionData) {
+      messages.push({ role: "system", content: originalPrompt });
+    } else {
+      messages.push(...(sessionData as any));
+    }
+    messages.push({ role: "user", content: inputText });
     const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      messages: messages,
       model: "gpt-3.5-turbo",
     });
-    text = chatCompletion.choices[0]?.message?.content as string;
+    responseText = chatCompletion.choices[0]?.message?.content as string;
+    messages.push({ role: "assistant", content: responseText });
+    await kv.set(sessionKey, JSON.stringify(messages));
   }
 
   const isEnded = false;
@@ -54,10 +63,7 @@ const handleRequest = frames(async (ctx) => {
   if (ctx.message?.transactionId) {
     return {
       image: (
-        <div
-          style={{ fontFamily: "Bitcell" }}
-          tw="bg-purple-800 text-white w-full h-full justify-center items-center flex"
-        >
+        <div style={{ fontFamily: "Bitcell" }} tw="bg-black text-white w-full h-full justify-center items-center flex">
           Transaction submitted! {ctx.message.transactionId}
         </div>
       ),
@@ -73,7 +79,7 @@ const handleRequest = frames(async (ctx) => {
   if (ctx.pressedButton && isEnded && isGenerated) {
     return {
       image: (
-        <div style={{ fontFamily: "Bitcell" }} tw="bg-purple-800 text-white w-full h-full justify-center items-center">
+        <div style={{ fontFamily: "Bitcell" }} tw="bg-black text-white w-full h-full justify-center items-center">
           Generated
         </div>
       ),
@@ -85,7 +91,7 @@ const handleRequest = frames(async (ctx) => {
   if (ctx.pressedButton && isEnded && !isGenerated) {
     return {
       image: (
-        <div style={{ fontFamily: "Bitcell" }} tw="bg-purple-800 text-white w-full h-full justify-center items-center">
+        <div style={{ fontFamily: "Bitcell" }} tw="bg-black text-white w-full h-full">
           Ended
         </div>
       ),
@@ -97,22 +103,30 @@ const handleRequest = frames(async (ctx) => {
   if (ctx.pressedButton && !isEnded) {
     return {
       image: (
-        <div style={{ fontFamily: "Bitcell" }} tw="bg-purple-800 text-white w-full h-full justify-center items-center">
-          {text}
+        <div style={{ fontFamily: "Bitcell", fontSize: 60 }} tw="flex bg-black text-white w-full h-full">
+          <p tw="px-8">{responseText}</p>
         </div>
       ),
       imageOptions,
       textInput: "What do you do?",
-      buttons: [<Button action="post">Next</Button>],
+      buttons: [
+        <Button action="post" target={{ query: { sessionKey } }}>
+          Next
+        </Button>,
+      ],
     };
   }
 
-  const newSessionKey = "sessionKey";
+  const newSessionKey = crypto.randomUUID();
+  console.log("newSessionKey", newSessionKey);
 
   return {
     image: (
-      <div style={{ fontFamily: "Bitcell" }} tw={`bg-gray-600 text-white w-full h-full justify-center items-center`}>
-        AI Quest 2
+      <div
+        style={{ fontFamily: "Bitcell", fontSize: 120 }}
+        tw={`flex bg-black text-white w-full h-full justify-center items-center`}
+      >
+        AI Quest
       </div>
     ),
     imageOptions,
