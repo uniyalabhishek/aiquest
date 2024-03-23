fcdbda28f0d4d768b92606326ea9c85a5a00c580
taijusanagi <taijusanagi@gmail.com>
2024-03-23 15:41:48 +0900

    add font

--- frames/app/frames/route.tsx
+++ frames/app/frames/route.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/jsx-key */
+import fs from "fs";
+import path from "path";
 import { Button } from "frames.js/next";
 import { createFrames } from "frames.js/next";
 import { kv } from "@vercel/kv";
@@ -12,6 +14,23 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const bitcell = fs.readFileSync(path.join(process.cwd(), "/public/bitcell_memesbruh03.ttf"));
+const fonts = [
+  {
+    name: "Bitcell",
+    data: bitcell,
+    weight: 400,
+    style: "normal",
+  },
+];
+const aspectRatio = "1.91:1";
+const imageOptions = {
+  aspectRatio,
+  fonts,
+} as any;
+
+// const vt323 = VT323({ subsets: ["latin"], weight: "400" });
+// className={vt323.className}
 const handleRequest = frames(async (ctx) => {
   const sessionKey = ctx.searchParams.sessionKey || "";
   console.log("sessionKey", sessionKey);
@@ -35,13 +54,14 @@ const handleRequest = frames(async (ctx) => {
   if (ctx.message?.transactionId) {
     return {
       image: (
-        <div tw="bg-purple-800 text-white w-full h-full justify-center items-center flex">
+        <div
+          style={{ fontFamily: "Bitcell" }}
+          tw="bg-purple-800 text-white w-full h-full justify-center items-center flex"
+        >
           Transaction submitted! {ctx.message.transactionId}
         </div>
       ),
-      imageOptions: {
-        aspectRatio: "1.91:1",
-      },
+      imageOptions,
       buttons: [
         <Button action="link" target={`https://www.onceupon.gg/tx/${ctx.message.transactionId}`}>
           View on block explorer
@@ -52,30 +72,36 @@ const handleRequest = frames(async (ctx) => {
 
   if (ctx.pressedButton && isEnded && isGenerated) {
     return {
-      image: <div tw="bg-purple-800 text-white w-full h-full justify-center items-center">Generated</div>,
-      imageOptions: {
-        aspectRatio: "1.91:1",
-      },
+      image: (
+        <div style={{ fontFamily: "Bitcell" }} tw="bg-purple-800 text-white w-full h-full justify-center items-center">
+          Generated
+        </div>
+      ),
+      imageOptions,
       buttons: [<Button action="post">Mint</Button>],
     };
   }
 
   if (ctx.pressedButton && isEnded && !isGenerated) {
     return {
-      image: <div tw="bg-purple-800 text-white w-full h-full justify-center items-center">Ended</div>,
-      imageOptions: {
-        aspectRatio: "1.91:1",
-      },
+      image: (
+        <div style={{ fontFamily: "Bitcell" }} tw="bg-purple-800 text-white w-full h-full justify-center items-center">
+          Ended
+        </div>
+      ),
+      imageOptions,
       buttons: [<Button action="post">Generate</Button>],
     };
   }
 
   if (ctx.pressedButton && !isEnded) {
     return {
-      image: <div tw="bg-purple-800 text-white w-full h-full justify-center items-center">{text}</div>,
-      imageOptions: {
-        aspectRatio: "1.91:1",
-      },
+      image: (
+        <div style={{ fontFamily: "Bitcell" }} tw="bg-purple-800 text-white w-full h-full justify-center items-center">
+          {text}
+        </div>
+      ),
+      imageOptions,
       textInput: "What do you do?",
       buttons: [<Button action="post">Next</Button>],
     };
@@ -84,10 +110,12 @@ const handleRequest = frames(async (ctx) => {
   const newSessionKey = "sessionKey";
 
   return {
-    image: <div tw="bg-purple-800 text-white w-full h-full justify-center items-center">AI Quest</div>,
-    imageOptions: {
-      aspectRatio: "1.91:1",
-    },
+    image: (
+      <div style={{ fontFamily: "Bitcell" }} tw={`bg-gray-600 text-white w-full h-full justify-center items-center`}>
+        AI Quest 2
+      </div>
+    ),
+    imageOptions,
     buttons: [
       <Button action="post" target={{ query: { sessionKey: newSessionKey } }}>
         Start
--- frames/app/layout.tsx
+++ frames/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { VT323 } from "next/font/google";
+const vt323 = VT323({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "AI Quest",
@@ -10,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={vt323.className}>{children}</body>
     </html>
   );
 }
