590e22fb4584e788aa3727c5d8b75edc05a69fe6
taijusanagi <taijusanagi@gmail.com>
2024-03-24 21:12:07 +0900

    added minimum home

--- frames/app/page.tsx
+++ frames/app/page.tsx
@@ -14,5 +14,19 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="min-h-screen flex flex-col bg-gradient-to-br from-gray-800 to-gray-900">
+      <div className="flex flex-col justify-center items-center pt-4 pb-12 px-4">
+        <div className="fixed inset-y-20 flex justify-center items-center">
+          <div className="text-center w-full max-w-2xl px-4">
+            <img src="./thumbnail.png" className="h-80 mx-auto mb-4" />
+            <p className="text-white font-bold text-xl mb-8">
+              AI Quest is a text-based RPG game built on the Farcaster framework. The adventure varies depending on both
+              on-chain and Farcaster activity.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
